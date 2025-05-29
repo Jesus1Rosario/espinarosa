a06e0fa7aa53083a1184ec792ce69b00a4762ac2
Yisussas <yisusdonw@gmail.com>
2025-05-28 22:45:31 -0500

    Se agrega el Proyecto

--- proyecto_UCC/crud_app/admin.py
+++ proyecto_UCC/crud_app/admin.py
@@ -1,20 +1,6 @@
 from django.contrib import admin
 from .models import Producto
-from django.utils.html import format_html
-
 
 admin.site.register(Producto)
 
 # Register your models here.
-from django.contrib import admin
-from .models import Producto
-
-@admin.register(Producto)
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'precio', 'imagen_tag')
-
-    def imagen_tag(self, obj):
-        if obj.imagen:
-            return format_html('<img src="{}" style="width: 100px; height:auto;" />', obj.imagen.url)
-        return "-"
-    imagen_tag.short_description = 'Imagen'
